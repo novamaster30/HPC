85466e651d8c2f571911be28914cef703c5c1d2b
novamaster30 <120588567+novamaster30@users.noreply.github.com>
2025-04-28 22:53:42 +0530

    Create hpc4py.py

---
+++ hpc4py.py
@@ -0,0 +1,59 @@
+pip install cupy
+
+import cupy as cp
+
+def initialize_vector(size):
+    return cp.random.randint(0, 10, size)
+
+def initialize_matrix(size):
+    return cp.random.randint(0, 10, (size, size))
+
+def add_vectors(A, B):
+    return A + B
+
+def multiply_matrices(A, B):
+    return A @ B  # or cp.matmul(A, B)
+
+def print_vector(vector):
+    print(' '.join(map(str, cp.asnumpy(vector))))
+
+def print_matrix(matrix):
+    for row in cp.asnumpy(matrix):
+        print(' '.join(map(str, row)))
+    print()
+
+def main():
+    N = 4
+
+    # Vector addition
+    A = initialize_vector(N)
+    B = initialize_vector(N)
+
+    print("Vector A:")
+    print_vector(A)
+    
+    print("Vector B:")
+    print_vector(B)
+
+    C = add_vectors(A, B)
+
+    print("Addition:")
+    print_vector(C)
+
+    # Matrix multiplication
+    D = initialize_matrix(N)
+    E = initialize_matrix(N)
+
+    print("\nMatrix D:")
+    print_matrix(D)
+
+    print("Matrix E:")
+    print_matrix(E)
+
+    F = multiply_matrices(D, E)
+
+    print("Multiplication:")
+    print_matrix(F)
+
+if _name_ == "_main_":
+    main()
